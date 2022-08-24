6fb20d880eaed54c45e3ecf4b4055007b53f16f9
NehadAwad <nehadakash622@gmail.com>
2022-08-24 11:11:02 +0600

    bug: create bug fixed

--- src/pages/Createpage.js
+++ src/pages/Createpage.js
@@ -1,32 +1,47 @@
 import { useState } from "react";
 
-const Create = () => {
-  const [title, setTitle] = useState('');
-  const [body, setBody] = useState('');
-  const [rating, setRating] = useState('1');
-  const [isPending, setPending] = useState('false');
-  const axios = require('axios').default;
 
+function Create(e){
+    const [restaurents, setRestaurents] = useState("")
+    const [date, setDate] = useState("")
+    const [image, setImage] = useState("")
+    const [location, setLocation] = useState("")
+    const [openningtime, setOpenningtime] = useState("")
+    const [closingtime, setClosingtime] = useState("")
 
+    const handleUpdate = async() => {
+    let data = {
+        title: restaurents,
+        date,
+        location,
+        openningtime,
+        closingtime
+    }
+    const image = document.querySelector('#image');
+    const formData = new FormData();
+    formData.append('data', JSON.stringify(data));
+    formData.append(`files.image`, image.files[0], image.files[0].name);
+    await fetch('https://starpy-backend.herokuapp.com/api/restaurents', {
+        method: 'post',
+        body: formData
+    });
+    }
 
-  function handleSubmit(e){
-      
-
-  return (
+    return (
     <div className="create">
-      <h2>Add a New Restaurant</h2>
-      <form>
+        <h2>Add a New Restaurant</h2>
+        <form>
         <table>
             <tbody>
-              <tr>
+                <tr>
                 <th>Restaurents</th>
-                <td><input type="text"/></td>
-              </tr>
-              <tr>
+                <td><input type="text"value={restaurents} onChange={(e) => {setRestaurents(e.target.value)}} /></td>
+                </tr>
+                <tr>
                 <th>Date</th>
-                <td><input type="date" /></td>
-              </tr>
-              <tr>
+                <td><input type="date" value={date} onChange={(e) => {setDate(e.target.value)}} /></td>
+                </tr>
+                <tr>
                 <th>Image</th>
                 <td>
                     <input type="file" name="image" id="image"/>
@@ -34,30 +49,45 @@ const Create = () => {
                 </tr>
                 <tr>
                 <th>Location</th>
-                <td><input type="text"/></td>
+                <td><input type="text" value={location} onChange={(e) => {setLocation(e.target.value)}} /></td>
                 </tr>
                 <tr>
                 <th>Openning Time</th>
                 <td>
-                  <input type="time"/>
+                    <input type="time" value={openningtime} onChange={(e) => {
+                      let value = e.target.value
+                      let value_split = value.split(':')
+                      if(value_split.length<3){
+                        value = value+':00'
+                      }
+                      setOpenningtime(value)
+                    }} />
                 </td>
                 </tr>
                 <tr>
                 <th>Closing Time</th>
-                <td><input type="time" /></td>
+                <td><input type="time" value={closingtime} onChange={(e) => {
+                  let value = e.target.value
+                  let value_split = value.split(':')
+                  if(value_split.length<3){
+                    value = value+':00'
+                  }
+                  setClosingtime(value)
+                  }}/></td>
                 </tr>
                 <tr>
                 <th>Action</th>
                 <td>
-                    <button type="button" >Create</button>
+                    <button type="button" onClick={handleUpdate}>Create</button>
                 </td>
                 </tr>
             </tbody>
             
         </table>
-      </form>
+        </form>
     </div>
-  );
+    );
+
 }
  
 export default Create;
